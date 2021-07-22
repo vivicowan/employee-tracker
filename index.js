7cfe7252b31d5e5134141f4dce035903ab619ad4
Vivi Cowan <vivicare@gmail.com>
2021-07-21 20:48:28 -0700

    created runOptions to prompt user with options

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const connection = mysql.createConnection({
  connection.connect((err) => {
 	if (err) throw err;
 	runBanner();
+	runOptions();
  });
  
 const runBanner = () => {
@@ -21,4 +22,80 @@ const runBanner = () => {
 	const banner_msg = chalk.bold.blue(figlet.textSync('Employee Tracker'));
 	const app_author = chalk.bold.white(`\n\n                           By: Vivianna Cowan\n`);
   	console.log(banner_wall + banner_msg + app_author + banner_wall);
-}
+};
+
+const runOptions = () => {
+	inquirer
+	.prompt({
+	  name: "option",
+	  type: "rawlist",
+	  message: "What would you like to do?",
+	  choices: [
+		'View all departments',
+		'View all Roles',
+		'View all employees',
+		'Add department',
+		'Add Roles',
+		'Add Employees',
+		'Delete Departments',
+		'Delete Employee',
+		'Delete Roles',
+		'Update Employee Roles',
+		'Update employee manager',
+		'View Employee By Manager',
+		'Exit',
+	  ],
+	})
+	.then((answer) => {
+		switch (answer.option) {
+			case "View all departments":
+				//  viewAllDepartments();
+				 break;
+
+			case "View all Roles":
+				//  viewAllRoles();
+				 break;
+
+			case "View all employees":
+				//  viewAllEmployees();
+				 break;
+
+			case "Add department":
+				//  addDepartment();
+				 break;
+
+			case "Add Roles":
+				//  addRoles();
+				 break;
+
+			case "Add Employees":
+				//  addEmployee();
+				 break;
+
+			case "Update Employee Roles":
+				//  updateEmployeeRole();
+				 break;
+
+			case "Delete Departments":
+				//  deleteDepartment();
+				 break;
+			case "Delete Employee":
+				//  deleteEmployee();
+				 break;
+			case "Delete Roles":
+				//  deleteRole();
+				 break;
+			case "Update employee manager":
+				//  updateManager()
+				 break;
+			case "View Employee By Manager":
+				//  viewEmployeeByManager()
+				 break;
+			case "Exit":
+				 connection.end();
+				 console.log('Have a good day');
+				 break;
+	   }
+	});
+
+};
