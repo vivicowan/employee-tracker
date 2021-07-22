d35be1dae9b18c12cadca06888f1b704f40bb229
Vivi Cowan <vivicare@gmail.com>
2021-07-21 21:45:03 -0700

    created Department, Role, and Employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,32 @@ CREATE database employee_trackerDB;
 
 USE employee_trackerDB;
 
+CREATE TABLE Department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR (50)NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50)NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES Department(id)
+
+);
+
+
+CREATE TABLE Employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR (30)NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES Role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    
+);
+
